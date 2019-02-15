16158275cbd239b441f19a31a08c0e5e4cfa1209
rosamaria <rosamaria.venditti@gmail.com>
2019-02-15 10:56:04 +0100

    First version of 3muons vertex

---
+++ DsTau3MuAna/DsTau3MuAna/python/__init__.py
@@ -0,0 +1,3 @@
+#Automatically created by SCRAM
+import os
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/DsTau3MuAna/DsTau3MuAna/',1)[0])+'/cfipython/slc6_amd64_gcc630/DsTau3MuAna/DsTau3MuAna')
